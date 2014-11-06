ba9527e8f1ff95a497bc29dc31a1cdf68a58edab
Chih-Wei Huang <cwhuang@linux.org.tw>
2014-11-06 10:58:59 +0800

    enable multiarch builds
    Convert LOCAL_MODULE_PATH to LOCAL_MODULE_RELATIVE_PATH
    to support 64-bit targets.

--- Android.mk
+++ Android.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2011-2013 The Android-x86 Open Source Project
+# Copyright (C) 2011-2014 The Android-x86 Open Source Project
 
 LOCAL_PATH := $(call my-dir)
 
@@ -6,36 +6,36 @@ LOCAL_PATH := $(call my-dir)
 # hw/<OVERLAY_HARDWARE_MODULE_ID>.<ro.product.board>.so
 include $(CLEAR_VARS)
 LOCAL_PRELINK_MODULE := false
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_SHARED_LIBRARIES := liblog libcutils
 LOCAL_MODULE := sensors.hdaps
 LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_SRC_FILES := hdaps.c
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_PRELINK_MODULE := false
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_SHARED_LIBRARIES := liblog libcutils
 LOCAL_MODULE := sensors.kbd
 LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_SRC_FILES := kbdsensor.cpp
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_PRELINK_MODULE := false
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_SHARED_LIBRARIES := liblog libcutils
 LOCAL_MODULE := sensors.s103t
 LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_SRC_FILES := s103t_sensor.c
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_PRELINK_MODULE := false
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_SHARED_LIBRARIES := liblog libcutils
 LOCAL_MODULE := sensors.w500
 LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_SRC_FILES := w500_sensor.c
 include $(BUILD_SHARED_LIBRARY)
